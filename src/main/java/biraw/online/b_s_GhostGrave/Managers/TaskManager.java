892facf8545c3fd0ce638a3f1e81ebcaa480657f
Bpetiii <87673385+Bpetiii@users.noreply.github.com>
2024-10-21 17:39:46 +0200

    Bugfix

--- src/main/java/biraw/online/b_s_GhostGrave/Managers/TaskManager.java
+++ src/main/java/biraw/online/b_s_GhostGrave/Managers/TaskManager.java
@@ -12,56 +12,63 @@ import org.bukkit.util.Vector;
 public class TaskManager {
 
     public int PlayTask(Bee ghost, Player player){
-
         int particle_count = B_s_GhostGrave.getConfigManager().particleCount;
         int[] taskId = new int[1];
         // Schedule a task to run every 2 ticks (1/10 second)
         taskId[0] = Bukkit.getScheduler().scheduleSyncRepeatingTask(B_s_GhostGrave.getPlugin(), () -> {
+            if (player != null && ghost != null){ // Check if it's valid to do the task
+                if (ghost.isDead()) Bukkit.getScheduler().cancelTask(taskId[0]);
 
-            if (ghost.isDead()) Bukkit.getScheduler().cancelTask(taskId[0]);
+                Location ghostLocation = ghost.getLocation().add(0,0.3,0);
 
-            Location ghostLocation = ghost.getLocation().add(0,0.3,0);
+                // Create particle effects
+                Particle.DustOptions smoke = new Particle.DustOptions(Color.BLACK, 8f);
+                Particle.DustOptions eye = new Particle.DustOptions(Color.WHITE, 0.6f);
 
-            // Create particle effects
-            Particle.DustOptions smoke = new Particle.DustOptions(Color.BLACK, 8f);
-            Particle.DustOptions eye = new Particle.DustOptions(Color.WHITE, 0.6f);
+                // Spawn smoke particle at ghost's location
+                ghost.getWorld().spawnParticle(Particle.DUST, ghostLocation, 2*particle_count, smoke);
 
-            // Spawn smoke particle at ghost's location
-            ghost.getWorld().spawnParticle(Particle.DUST, ghostLocation, 2*particle_count, smoke);
+                // Setting ai false
+                ghost.setAI(false);
 
-            // Setting ai false
-            ghost.setAI(false);
+                //Check if the dimension of the player is the same as the ghosts (needed for distance calculation)
+                if (ghost.getWorld() == player.getWorld())
+                {
+                    // Check distance between ghost and player
+                    if (ghostLocation.distance(player.getLocation()) < 20 && ghostLocation.distance(player.getLocation()) > 3) {
+                        // Disable "bee" behavior
+                        ghost.setFlower(null);
+                        ghost.setCannotEnterHiveTicks(100);
+                        ghost.setHive(null);
+                        ghost.setAI(true);
 
-            // Check distance between ghost and player
-            if (ghostLocation.distance(player.getLocation()) < 20 && ghostLocation.distance(player.getLocation()) > 3) {
-                // Disable "bee" behavior
-                ghost.setFlower(null);
-                ghost.setCannotEnterHiveTicks(100);
-                ghost.setHive(null);
-                ghost.setAI(true);
+                        // Move the ghost toward the player
+                        ghost.getPathfinder().moveTo(player.getLocation().add(player.getFacing().getModX() * 2.0, 1.0, player.getFacing().getModZ() * 2.0));
+                    }
+                }
 
-                // Move the ghost toward the player
-                ghost.getPathfinder().moveTo(player.getLocation().add(player.getFacing().getModX() * 2.0, 1.0, player.getFacing().getModZ() * 2.0));
+
+                // GHOST EYES
+
+                // Getting the ghosts looking direction
+                Vector ghostVN = player.getLocation().add(0,1,0).toVector().subtract(ghostLocation.toVector()).normalize();
+
+                // Rotate the vector by +5 degrees and -5 degrees for each eye
+                Vector ghostEyeOneDirection = rotateVector(ghostVN, 45);
+                Vector ghostEyeTwoDirection = rotateVector(ghostVN, -45);
+
+                // Calculate the positions for the eyes
+                Location ghostEyeOne = ghostLocation.clone().add(ghostEyeOneDirection.multiply(0.7));
+                Location ghostEyeTwo = ghostLocation.clone().add(ghostEyeTwoDirection.multiply(0.7));
+
+                // Spawn eye particles
+                // Also add a little elevation, it looks better
+                ghost.getWorld().spawnParticle(Particle.DUST, ghostEyeOne.add(0,0.2,0), particle_count, eye);
+                ghost.getWorld().spawnParticle(Particle.DUST, ghostEyeTwo.add(0,0.2,0), particle_count, eye);
+            }else
+            {
+                B_s_GhostGrave.getPlugin().getLogger().warning("Ghost not working properly at: "+ghost.getLocation());
             }
-
-            // GHOST EYES
-
-            // Getting the ghosts looking direction
-            Vector ghostVN = player.getLocation().add(0,1,0).toVector().subtract(ghostLocation.toVector()).normalize();
-
-            // Rotate the vector by +5 degrees and -5 degrees for each eye
-            Vector ghostEyeOneDirection = rotateVector(ghostVN, 45);
-            Vector ghostEyeTwoDirection = rotateVector(ghostVN, -45);
-
-            // Calculate the positions for the eyes
-            Location ghostEyeOne = ghostLocation.clone().add(ghostEyeOneDirection.multiply(0.7));
-            Location ghostEyeTwo = ghostLocation.clone().add(ghostEyeTwoDirection.multiply(0.7));
-
-            // Spawn eye particles
-            // Also add a little elevation, it looks better
-            ghost.getWorld().spawnParticle(Particle.DUST, ghostEyeOne.add(0,0.2,0), particle_count, eye);
-            ghost.getWorld().spawnParticle(Particle.DUST, ghostEyeTwo.add(0,0.2,0), particle_count, eye);
-
         }, 0L, 2L);
 
         return taskId[0];
